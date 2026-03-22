9443be0bb1bcdfea929d7c6a542de5453ffb5c95
kelleno96 <kellen.oconnor01@gmail.com>
2026-03-21 19:13:46 -0700

    Initial Commit

---
+++ VibeCheck/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  VibeCheck
+//
+//  Created by Kellen O'Connor on 3/21/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ VibeCheckTests/VibeCheckTests.swift
@@ -0,0 +1,16 @@
+//
+//  VibeCheckTests.swift
+//  VibeCheckTests
+//
+//  Created by Kellen O'Connor on 3/21/26.
+//
+
+import Testing
+
+struct VibeCheckTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
